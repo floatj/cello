88840b7c2f1d61f14b33d2765fb9fa3592e8b093
FloatJ <floatj@gmail.com>
2017-03-27 01:08:36 +0800

    增加平台判斷, 分離密碼

--- run_mariadb.sh
+++ run_mariadb.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+
+mysql_password='cello@mysql#5566'
+
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+   home_dir='/home/floatj/docker_volume/mariadb'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='mac os'
+   home_dir='/Users/johnny_liao/docker_volume/maraidb'
+fi
+
+
+
 docker run --restart=always -d --name cello_mariadb \
      	-p 3306:3306  \
-	-e MYSQL_ROOT_PASSWORD=123456 \
-	-e MYSQL_DATABASE=homestead \
+	-e MYSQL_ROOT_PASSWORD=${mysql_password} \
+	-e MYSQL_DATABASE=cello \
 	-e MYSQL_USER=abctest \
-	-e MYSQL_PASSWORD=test5566 \
-	-v /Users/johnny_liao/docker_volume/maraidb:/var/lib/mysql \
+	-e MYSQL_PASSWORD=test@5566 \
+	-v ${home_dir}:/var/lib/mysql \
 	mariadb:10.1
