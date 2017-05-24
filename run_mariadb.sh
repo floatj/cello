97284f5b4afa9994225ae96f7c4d9348023d6c36
FloatJ <floatj@gmail.com>
2017-05-24 20:18:01 +0800

    Delete run_mariadb.sh

--- run_mariadb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --restart=always -d --name cello_mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_DATABASE=homestead -e MYSQL_USER=homestead -e MYSQL_PASSWORD=secret mariadb:10.1
-
