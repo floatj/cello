66c832882b0df44a31f20c82473aeb267ca6b2b6
FloatJ <floatj@gmail.com>
2017-05-24 21:04:40 +0800

    Delete run_nginx.sh

--- run_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker run \
--d --name cello_nginx \
--p 8099:80 \
-#--link cello_phpfpm:phpfpm \
--v /home/johnny/web/blog:/var/project \
--v /home/johnny/project/cello/nginx_default.conf:/etc/nginx/conf.d/default.conf \
-nginx:1.11.10
